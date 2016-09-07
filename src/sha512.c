eff52e3c5ea26c341abca2eaffdfae5aaba4437e
sparticvs <sparticvs@popebp.com>
2016-09-07 12:03:38 -0400

    Remove SHA-256 extras from SHA-512 source

--- src/sha512.c
+++ src/sha512.c
@@ -329,13 +329,13 @@ static inline void __blk_htobe(uint64_t *data) {
  *
  * @return Returns an RV_ value
  */
-rv_t sha256_update(hash_ctx_t *hash_ctx, uint8_t *data, uint64_t len) {
+rv_t sha512_update(hash_ctx_t *hash_ctx, uint8_t *data, uint64_t len) {
     rv_t rv = RV_UNKNOWN;
 
     if (NULL == hash_ctx || NULL == data) {
         rv = RV_INVALARG;
     } else {
-        sha256_ctx_t *ctx = (sha256_ctx_t*)hash_ctx;
+        sha512_ctx_t *ctx = (sha512_ctx_t*)hash_ctx;
         // 1. If ctx->pos > 0, fill blk up and process, memmove data and change
         // length
         if (ctx->pos > 0) {
@@ -351,7 +351,7 @@ rv_t sha256_update(hash_ctx_t *hash_ctx, uint8_t *data, uint64_t len) {
 
             if (ctx->pos == 64) {
                 __blk_htobe(ctx->blk);
-                __sha256_compute(ctx->common.hash, ctx->blk);
+                __sha512_compute(ctx->common.hash, ctx->blk);
                 ctx->tot += avail;
 
                 memset(ctx->blk, 0, 16 * sizeof(uint32_t));
@@ -375,7 +375,7 @@ rv_t sha256_update(hash_ctx_t *hash_ctx, uint8_t *data, uint64_t len) {
         uint64_t cnt;
         for (cnt = 0; cnt < len / 64; cnt++) {
             __blk_htobe((void*)data+(64*cnt));
-            __sha256_compute(ctx->common.hash, (void*)data+(64*cnt));
+            __sha512_compute(ctx->common.hash, (void*)data+(64*cnt));
             ctx->tot += 64;
         }
 
@@ -386,22 +386,22 @@ rv_t sha256_update(hash_ctx_t *hash_ctx, uint8_t *data, uint64_t len) {
     return rv;
 }
 
-/* SHA-256 Finalize
+/* SHA-512 Finalize
  *
- * Finish the SHA-256 computation. This pads the blocks correctly and does the
+ * Finish the SHA-512 computation. This pads the blocks correctly and does the
  * final update to the hash.
  *
- * @param ctx       Pointer to the sha-256 context to finalize
+ * @param ctx       Pointer to the sha-512 context to finalize
  *
  * @return Returns an RV_ value
  */
-rv_t sha256_finalize(hash_ctx_t *hash_ctx) {
+rv_t sha512_finalize(hash_ctx_t *hash_ctx) {
     rv_t rv = RV_UNKNOWN;
 
     if (NULL == hash_ctx) {
         rv = RV_INVALARG;
     } else {
-        sha256_ctx_t *ctx = (sha256_ctx_t*)hash_ctx;
+        sha512_ctx_t *ctx = (sha512_ctx_t*)hash_ctx;
         // 1. Append 0x80 to the block and update the pos
         memset((void*)ctx->blk+ctx->pos, 0x80, 1);
         ctx->pos += 1;
@@ -409,7 +409,7 @@ rv_t sha256_finalize(hash_ctx_t *hash_ctx) {
         // 2. If pos > 57, compute block, and clear
         if (ctx->pos > 57) {
             __blk_htobe(ctx->blk);
-            __sha256_compute(ctx->common.hash, ctx->blk);
+            __sha512_compute(ctx->common.hash, ctx->blk);
 
             memset(ctx->blk, 0, 16 * sizeof(uint32_t));
             ctx->pos = 0;
@@ -421,7 +421,7 @@ rv_t sha256_finalize(hash_ctx_t *hash_ctx) {
 
         // 4. compute the last block
         __blk_htobe(ctx->blk);
-        __sha256_compute(ctx->common.hash, ctx->blk);
+        __sha512_compute(ctx->common.hash, ctx->blk);
  
         // 5. return success
         rv = RV_SUCCESS;
@@ -435,106 +435,3 @@ void sha512_print(hash_ctx_t *ctx, const char *fname) {
 
 void sha512_print_bsd(hash_ctx_t *ctx, const char *fname) {
 }
-
-//int main(int argc, char **argv) {
-//    //unit_tests();
-//    const char *filename = "-";
-//    FILE *fp = stdin;
-//    uint32_t i;
-//    struct stat sb;
-//
-//    for (i = 1; i < argc; i++) {
-//        filename = argv[i];
-// 
-//        if(stat(filename, &sb) == 0 && !S_ISREG(sb.st_mode)) {
-//            continue;
-//        }
-//        fp = fopen(filename, "r");
-//
-//        if(ferror(fp)) {
-//            // Unable to operate on it, error
-//            perror("opening");
-//            continue;
-//        }
-//
-////    if(argc > 1) {
-////        // We got a file, yes?
-////        filename = argv[1];
-////        fp = fopen(filename, "r");
-////    }
-//        uint8_t array[ARRAY_SZ] = {0};
-//        uint64_t actual = 0;
-//        hash_ctx_t ctx;
-//        sha256_initialize(&ctx);
-//        while (!feof(fp)) {
-//            memset(array, 0, sizeof(uint8_t) * ARRAY_SZ);
-//            actual = fread(array, 1, sizeof(uint8_t) * ARRAY_SZ, fp);
-//            sha256_update(&ctx, array, actual);
-//        }
-//        sha256_finalize(&ctx);
-//        fclose(fp);
-//        printf(DEFAULT_OUTPUT, ctx.h[0], ctx.h[1], ctx.h[2], ctx.h[3], ctx.h[4], ctx.h[5], ctx.h[6], ctx.h[7], ' ', filename);
-//    }
-//
-//    return 0;
-//}
-#define NESSIE_TV_MAX 8
-const testvector_t NESSIE_SHA256_TV[] = {
-    { // Set 1, Vector 0
-        .tv = "",
-        .len = 0,
-        .hash = { 
-            0xE3B0C442, 0x98FC1C14, 0x9AFBF4C8, 0x996FB924,
-            0x27AE41E4, 0x649B934C, 0xA495991B, 0x7852B855 }
-    },
-    { // Set 1, Vector 1
-        .tv = "a",
-        .len = 1,
-        .hash = {
-            0xCA978112, 0xCA1BBDCA, 0xFAC231B3, 0x9A23DC4D,
-            0xA786EFF8, 0x147C4E72, 0xB9807785, 0xAFEE48BB }
-    },
-    { // Set 1, Vector 2
-        .tv = "abc",
-        .len = 3,
-        .hash = {
-            0xBA7816BF, 0x8F01CFEA, 0x414140DE, 0x5DAE2223,
-            0xB00361A3, 0x96177A9C, 0xB410FF61, 0xF20015AD }
-    },
-    { // Set 1, Vector 3
-        .tv = "message digest",
-        .len = 14,
-        .hash = {
-            0xF7846F55, 0xCF23E14E, 0xEBEAB5B4, 0xE1550CAD,
-            0x5B509E33, 0x48FBC4EF, 0xA3A1413D, 0x393CB650 }
-    },
-    { // Set 1, Vector 4
-        .tv = "abcdefghijklmnopqrstuvwxyz",
-        .len = 26,
-        .hash = {
-            0x71C480DF, 0x93D6AE2F, 0x1EFAD144, 0x7C66C952,
-            0x5E316218, 0xCF51FC8D, 0x9ED832F2, 0xDAF18B73 }
-    },
-    { // Set 1, Vector 5
-        .tv = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq",
-        .len = 56,
-        .hash = {
-            0x248d6a61, 0xd20638b8, 0xe5c02693, 0x0c3e6039,
-            0xa33ce459, 0x64ff2167, 0xf6ecedd4, 0x19db06c1 }
-    },
-    { // Set 1, Vector 6
-        .tv = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-        .len = 62,
-        .hash = {
-            0xDB4BFCBD, 0x4DA0CD85, 0xA60C3C37, 0xD3FBD880,
-            0x5C77F15F, 0xC6B1FDFE, 0x614EE0A7, 0xC8FDB4C0 }
-    },
-    { // Set 1, Vector 7
-        .tv = "12345678901234567890123456789012345678901234567890123456789012345678901234567890",
-        .len = 80,
-        .hash = {
-            0xF371BC4A, 0x311F2B00, 0x9EEF952D, 0xD83CA80E,
-            0x2B60026C, 0x8E935592, 0xD0F9C308, 0x453C813E }
-    },
-    /** Skipping remainder, as the last one is 1 million `a's **/
-};
